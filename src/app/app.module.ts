c1b833abfeb0ca1d13f07c0d619f308be027d1a2
tuankietdut <tuankietdut@gmail.com>
2021-11-02 10:01:58 +0700

    modifier manager_teacher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
+
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {ShareModule} from "./share-module/share.module";
-import {TeacherModule} from "./feature-module/teacher/teacher.module";
-
-
-
-
-import {environment} from "../environments/environment";
-import {AngularFireStorageModule} from "@angular/fire/storage";
-import {AngularFireModule} from "@angular/fire";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-
   ],
-  // imports: [
-  //   BrowserModule,
-  //   AppRoutingModule,
-  //   ShareModule,
-  //   TeacherModule,
-  //   StudentModule
-  // ],
-
-
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ShareModule,
-        TeacherModule,
-      AngularFireStorageModule,
-      AngularFireModule.initializeApp(environment.firebaseConfig, "cloud"),
-    ],
 
   providers: [],
   bootstrap: [AppComponent]
--- src/app/share-module/share.module.ts
+++ src/app/share-module/share.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
 import {DialogDeleteComponent} from "./dialog-delete/dialog-delete.component";
 import {SnackbarComponent} from "./snackbar/snackbar.component";
 import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
-import {HttpClientModule} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {RouterLink, RouterLinkActive, RouterModule} from "@angular/router";
-import {AppRoutingModule} from "../app-routing.module";
-import {AngularFireStorage} from "@angular/fire/storage";
 
 
 
@@ -24,33 +14,6 @@ import {AngularFireStorage} from "@angular/fire/storage";
     FooterComponent,
     HeaderComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MatSnackBarModule,
-    MatDialogModule,
-    MatButtonModule,
-    RouterModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
 
-  ],
-  exports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MatSnackBarModule,
-    MatDialogModule,
-    MatButtonModule,
-    HeaderComponent,
-    FooterComponent,
-    DialogDeleteComponent,
-    HttpClientModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
-  ]
 })
 export class ShareModule { }
